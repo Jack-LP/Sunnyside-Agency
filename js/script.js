d507bcb9bfeec9704888d12902fb030f4ad94a24
Jack Paget <90701888+Jack-LP@users.noreply.github.com>
2021-09-15 20:25:52 +0100

    Responsive design, added js

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const hamburger = document.getElementById("hamburger");
+const navUL = document.getElementById("nav-ul");
+
+hamburger.addEventListener("click", () => {
+    navUL.classList.toggle("show");
+});
+
